6cbf4faf07c5441e07a806adbeb9ae1971a8054d
asif ansari <ashifa454@gmail.com>
2019-08-13 01:52:49 +0530

    search bar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'githubExplorer';
+  isSearchActive = false;
+  toggleSearchbar() {
+    this.isSearchActive = !this.isSearchActive;
+  }
 }
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -25,5 +25,8 @@ export class UserDetailsComponent implements OnInit {
       });
     })
   }
+  showInfo(link) {
 
+    window.open(link, '_blank');
+  }
 }
